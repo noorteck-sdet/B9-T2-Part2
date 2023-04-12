c587dbbc769c015427699744d56208a29c0a0b29
DELL <DELL@10.0.0.179>
2023-04-12 13:06:26 -0400

    pushing my info

---
+++ src/pushingToRemote/homeworkPart2.java
@@ -0,0 +1,19 @@
+package pushingToRemote;
+
+public class homeworkPart2 {
+
+	public static void main (String args []) {
+		
+	}
+		
+		public void  getAge() {
+			
+			
+			System.out.println("first push to remote");
+		}
+	
+	public void setName() {
+		System.out.println("making changues");
+	}
+	
+}
