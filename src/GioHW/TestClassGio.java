ffa25a3e03f23883ae550f640b186e90528c2647
Clavi <Clavi@DESKTOP-2O92DCN.mynetworksettings.com>
2023-04-11 21:14:08 -0400

    gios method

---
+++ src/GioHW/TestClassGio.java
@@ -0,0 +1,12 @@
+package GioHW;
+
+public class TestClassGio {
+	
+	public void Method1() {
+		System.out.println("Gio's first method");
+	}
+	
+	public void Method2() {
+		System.out.println("Gio's second method");
+	}
+}
