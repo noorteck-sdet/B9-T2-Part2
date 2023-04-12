933c7936d7a85ad1cfb9af5158d771a4cc5cff23
MehrnazL <129793229+MehrnazL@users.noreply.github.com>
2023-04-12 18:21:01 -0400

    B9 T2

---
+++ src/Naz/HWGroupProject.java
@@ -0,0 +1,18 @@
+package Naz;
+
+public class HWGroupProject {
+
+	public static void main(String[] args) {
+
+	}
+
+	public void firstmMethod() {
+		System.out.println("first Test");
+
+	}
+
+	public void secondMethod() {
+		System.out.println("Test 2");
+
+}
+}
