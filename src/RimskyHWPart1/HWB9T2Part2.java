e469e3ec34c1ee151a08a027e33b5f1b48f9d55e
meno <meno@menos-mbp.hsd1.mi.comcast.net>
2023-04-12 18:15:56 -0400

    Meno test2 in Rimsky HW

--- src/RimskyHWPart1/HWB9T2Part2.java
+++ src/RimskyHWPart1/HWB9T2Part2.java
@@ -14,10 +14,16 @@ public class HWB9T2Part2 {
 		System.out.println("Second code.....");
 
 	}
+
 	public void thirdMethod() {
 		System.out.println("second code...");
 	}
+
 	public void Luis() {
 		System.out.println("added a line of code");
+	}
+
+	public static void Meno() {
+		System.out.println("TEST #2");
 	}
 }
