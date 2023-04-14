c193307ad68af7ff04f2d2ea9637c1c1456cf146
sejon <sejon@LAPTOP-GI3U8FMJ.fios-router.home>
2023-04-14 12:36:54 -0400

    Initial commit

---
+++ src/serenahw/SerenaHomework.java
@@ -0,0 +1,14 @@
+package serenahw;
+
+public class SerenaHomework {
+public static void main(String[] args) {
+	
+	System.out.println("First line of code");
+}
+public void getInfo( int a, int b) {
+	System.out.println(a + b);
+}
+public void getInfo2( int a, int b, int c) {
+	System.out.println(a * b * c);
+}
+}
